58f8a247b5ccb6588f4f73bb903a3e8c10c6d41d
dimaorenru <dima@oren.ru>
2022-01-04 01:07:02 +0500

    Added fromWindows

---
+++ Variable.java
@@ -0,0 +1,31 @@
+public class Variable {
+
+    public static void main(String[] args) {
+        byte cpu = 1 + 3;
+
+        System.out.println(cpu);
+        short ram = 16;
+
+        System.out.println(ram);
+        int ssd = 1024;
+
+        System.out.println(ssd);
+        long hdd = 1024;
+
+        System.out.println(hdd);
+        float ghz = 3.4f;
+
+        System.out.println(ghz);
+        double dimm = 1.333;
+
+        System.out.println(dimm);
+        char intel = 'i';
+
+        System.out.println(intel);
+        int gpu = 4;
+        int ddr = 3;
+        boolean ddr3 = gpu > ddr;
+
+        System.out.println(ddr3);
+}   
+    }
