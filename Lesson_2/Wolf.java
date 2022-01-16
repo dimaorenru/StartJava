32bbbf45bc1154b6e213a46eb7141b6795f85151
dimaorenru <97004387+dimaorenru@users.noreply.github.com>
2022-01-16 21:57:27 +0500

    added hw Wolf

---
+++ Lesson_2/Wolf.java
@@ -0,0 +1,24 @@
+public class Wolf {
+
+    String nickName = "Balto";
+    String gender = "male";
+    String color = "grey";
+    int weight = 45;
+    int age = 4;
+
+    void run() {
+        System.out.println("Бежит");
+    }
+
+    void howl() {
+        System.out.println("Воет");
+    }
+
+    void sit() {
+        System.out.println("Сидит");
+    }
+
+    void hunt() {
+        System.out.println("Охотится");
+    }
+}
