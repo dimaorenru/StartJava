c327979ffffd4d8a4cc287e44fbbc88a28a085b9
dimaorenru <97004387+dimaorenru@users.noreply.github.com>
2022-01-16 23:46:07 +0500

    fix Wolf

---
+++ Lesson_2/Wolf.java
@@ -0,0 +1,24 @@
+public class Wolf {
+
+    String nickName;
+    String gender;
+    String color;
+    int weight;
+    int age;
+
+    void run() {
+        System.out.println("Бежит");
+    }
+
+    void howl() {
+        System.out.println("Воет");
+    }
+
+    void sit() {
+        System.out.println("Сидит");
+    }
+
+    void hunt() {
+        System.out.println("Охотится");
+    }
+}
