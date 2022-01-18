14b06b7431d3102a2b23d42d9080f1d9289858f4
dimaorenru <97004387+dimaorenru@users.noreply.github.com>
2022-01-18 18:09:27 +0500

    update hm Wolf

---
+++ Lesson_2/Wolf.java
@@ -0,0 +1,68 @@
+public class Wolf {
+
+    private String nickName;
+    private String gender;
+    private String color;
+    private int weight;
+    private int age;
+
+    public void run() {
+        System.out.println("Бежит");
+    }
+
+    public void howl() {
+        System.out.println("Воет");
+    }
+
+    public void sit() {
+        System.out.println("Сидит");
+    }
+
+    public void hunt() {
+        System.out.println("Охотится");
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 8) {
+            System.out.println("Неправильный возраст");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
