1fd6d6b35137ca4ddc42ab38a2b4592e3190f692
dimaorenru <97004387+dimaorenru@users.noreply.github.com>
2022-02-22 02:12:20 +0500

    добавил игру

--- src/com/startjava/Lesson_2_3_4/game/GuessNumber.java
+++ src/com/startjava/Lesson_2_3_4/game/GuessNumber.java
@@ -1,47 +1,71 @@
 package com.startjava.Lesson_2_3_4.game;
 
-import java.util.Scanner;
 import java.util.Random;
+import java.util.Scanner;
 
 public class GuessNumber {
-    Scanner scanner = new Scanner(System.in);
+    Random random = new Random();
     private int guessNumber;
     private Player player1;
     private Player player2;
-    public int attempt;
 
     public GuessNumber(Player player1, Player player2) {
         this.player1 = player1;
         this.player2 = player2;
-        Random random = new Random();
-        guessNumber = random.nextInt(100) + 1;
     }
 
-    public void start() {
-        while(true) {
-            attempt++;
-            if (makeMove(player1.getName())) {
+    void start() {
+        guessNumber = random.nextInt(101);
+        System.out.println("Введите числа от 0 до 100. У вас 10 попыток");
+        int attempt;
+        final int maxNumberAttempt = 10;
+        for (attempt = 0; attempt < maxNumberAttempt; attempt++) {
+            inputNumber(player1, attempt);
+            if (checkNumber(player1, attempt)) {
+                attempt++;
                 break;
             }
-            if (makeMove(player2.getName())) {
+            inputNumber(player2, attempt);
+            if (checkNumber(player2, attempt)) {
+                attempt++;
                 break;
             }
         }
+        if (attempt == maxNumberAttempt) {
+            System.out.println("У " + player1.getName() + " закончились попытки");
+            System.out.println("У " + player2.getName() + " закончились попытки");
+            System.out.println("Загаданное число - " + guessNumber);
+        }
+        outputNumbers(player1.getNumbers(attempt));
+        outputNumbers(player2.getNumbers(attempt));
+        player1.resetArray(attempt);
+        player2.resetArray(attempt);
     }
 
-    private boolean makeMove(String playerName) {
-        System.out.println("Попытка № " + attempt + " игрока - " + playerName);
-        System.out.println("Введите число");
-        int inputNumber = scanner.nextInt();
-        if (inputNumber == guessNumber) {
-            System.out.println("Выиграл " + playerName);
+    private void inputNumber(Player player, int index) {
+        System.out.println("Попытка " + (index + 1) + " игрока " + player.getName());
+        int number = new Scanner(System.in).nextInt();
+        if (number < 0 || number > 100) {
+            System.out.println("Вы ввели число вне указанного диапазона");
+        }
+        player.setNumber(number, index);
+    }
+
+    private boolean checkNumber(Player player, int index) {
+        if (player.getNumber(index) == guessNumber) {
+            System.out.println("Игрок " + player.getName() + " угадал число " + player.getNumber(index) + " с " + (index + 1) + " попытки");
             return true;
-        }
-        if (inputNumber > guessNumber) {
-            System.out.println("Введенное вами число больше того, что загадал компьютер");
         } else {
-            System.out.println("Введенное вами число меньше того, что загадал компьютер");
+            String s = player.getNumber(index) > guessNumber ? "больше" : "меньше";
+            System.out.println("Введенное вами число " + s + " того, что загадал компьютер");
+            return false;
         }
-        return false;
+    }
+
+    private void outputNumbers(int[] numbers) {
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+        System.out.println();
     }
 }
--- src/com/startjava/Lesson_2_3_4/game/Player.java
+++ src/com/startjava/Lesson_2_3_4/game/Player.java
@@ -1,13 +1,32 @@
 package com.startjava.Lesson_2_3_4.game;
 
+import java.util.Arrays;
+
 public class Player {
     private String name;
-    
+    private int[] numbers = new int[10];
+
     public Player(String name) {
         this.name = name;
     }
-    
+
     public String getName() {
         return name;
+    }
+
+    public void setNumber(int number, int index) {
+        numbers[index] = number;
+    }
+
+    public int[] getNumbers(int index) {
+        return Arrays.copyOf(numbers, index);
+    }
+
+    public int getNumber(int index) {
+        return numbers[index];
+    }
+
+    public void resetArray(int index) {
+        Arrays.fill(numbers, 0, index, 0);
     }
 }
